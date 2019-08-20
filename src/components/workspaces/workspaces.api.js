4cb7db6f7a03292ea4c6db2b57df2e159f688402
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-08-20 16:59:20 +0200

    Add endpoint for listing workspaces

---
+++ src/components/workspaces/workspaces.api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { listWorkSpaces } = require('../../service/watson');
+
+const routes = () => {
+  const router = express.Router();
+
+  router.get('/', async (req, res) => {
+    try {
+      const result = await listWorkSpaces();
+
+      return res.json(result);
+    } catch (err) {
+      // Send back error in json.
+      return res.status(err.status || 500).json(err);
+    }
+  });
+
+  return router;
+};
+
+module.exports = routes;
