dd0db82810444c6fe46ae97e823b2fe2ab7f221a
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-08-21 11:04:43 +0200

    Add general validator for GUID’s

--- src/validation/global.schema.js
+++ src/validation/global.schema.js
@@ -2,9 +2,11 @@ const Joi = require('@hapi/joi');
 
 // Write all your general Joi-specifications here so they can be imported to all schemas.
 const id = Joi.string().min(5).max(24);
+const guid = Joi.string().min(36).max(36);
 const limit = Joi.number().min(1).max(100).default(10);
 
 module.exports = {
   id,
   limit,
+  guid,
 };
