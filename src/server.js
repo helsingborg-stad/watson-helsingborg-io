bf1c404159f2fc42d91e22f52896524fb7cb03e7
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-08-21 11:21:21 +0200

    Disable some boilerplate features & configure env vars etc

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
+
+
 const express = require('express');
-const config = require('config');
-const https = require('https');
+// const config = require('config');
+// TODO: Configure HTTPS properly, need to know deployment enviroment to do this.
+// const https = require('https');
+const http = require('http');
 const pino = require('express-pino-logger');
-const swaggerUi = require('swagger-ui-express');
+// const swaggerUi = require('swagger-ui-express');
 const bodyParser = require('body-parser');
-const swaggerDocument = require('../swagger/swagger.js');
+// const swaggerDocument = require('../swagger/swagger.js');
 const routes = require('./components/routes');
 const logger = require('./utils/logger');
-const WebSocketServer = require('./ws.server');
+// const WebSocketServer = require('./ws.server');
 
 /**
  * Config
  */
-const SERVER_PORT = config.get('SERVER.PORT');
+const { SERVER_PORT } = process.env;
 const API_BASE = '/api/v1';
 
 /**
@@ -30,15 +40,16 @@ app.use(pino({ logger }));
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use(API_BASE, routes());
 
+// TODO: Document endpoints using swagger
 // Swagger for documenting the api, access through localhost:xxxx/api-docs.
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Create WebSocket server
  */
-const webSocketServer = new WebSocketServer(server, `${API_BASE}/ws`);
+// const webSocketServer = new WebSocketServer(server, `${API_BASE}/ws`);
 
 /**
  * Start
@@ -47,7 +58,7 @@ const webSocketServer = new WebSocketServer(server, `${API_BASE}/ws`);
 // Listen on port specfied in env-file.
 server.listen({ port: SERVER_PORT }, async () => {
   logger.info(`Server started on port ${SERVER_PORT}`);
-  webSocketServer.start();
+  // webSocketServer.start();
 });
 
 // Export server to use it in tests.
