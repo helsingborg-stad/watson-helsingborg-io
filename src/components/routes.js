621bd1bcb87bcb2b5e2ca4f812cf943a78e4d87d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-08-21 11:03:37 +0200

    Remove boilerplate routes

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,13 +1,11 @@
 const express = require('express');
 
-const person = require('./person/person.api');
 const workspaces = require('./workspaces/workspaces.api');
 
 const routes = () => {
   const router = express.Router();
 
   // Register route to api-layer.
-  router.use('/person', person());
   router.use('/workspaces', workspaces());
 
   return router;
