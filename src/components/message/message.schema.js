10eba92346b773f8188aa7ff267582912bfd2037
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-09-16 09:24:00 +0200

    update post schema API to include conversation ID

--- src/components/message/message.schema.js
+++ src/components/message/message.schema.js
@@ -4,7 +4,9 @@ const { guid } = require('../../validation/global.schema');
 // Generic Schema.
 const postSchema = Joi.object().keys({
   textInput: Joi.string().required(),
-  context: Joi.string(),
+  context: Joi.object().keys({
+    conversation_id: Joi.string(),
+  }),
   workspaceId: guid,
 });
 
