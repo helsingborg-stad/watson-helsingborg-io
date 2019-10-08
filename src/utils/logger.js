74134128ed1c8a0c90e2afba17bde4a157700940
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-10-08 11:13:36 +0200

    Replace log level config.get with env var

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,4 @@
 const pino = require('pino');
 const config = require('config');
 
-const LOG_LEVEL = config.get('LOG.LEVEL');
-
-module.exports = pino({ level: LOG_LEVEL });
+module.exports = pino({ level: process.env.LOG_LEVEL });
